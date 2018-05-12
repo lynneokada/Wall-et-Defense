45cbf26482c31ca3b189737223b91e376fe964aa
Taylor Dinwiddie <taylordinwiddie@gmail.com>
2018-05-11 23:31:07 -0700

    Including the tile map and making it fit the browser

--- game/js/states/gamePlay.js
+++ game/js/states/gamePlay.js
@@ -3,16 +3,39 @@
 var gamePlayState = {
 	preload: function() {
 		game.load.atlas('gameAtlas', 'assets/img/spriteatlas.png', 'assets/img/sprites.json', Phaser.Loader.TEXTURE_ATLAS_JSON_HASH);
+		game.load.tilemap('levelOne', 'assets/img/WTTileMapOne.json', null, Phaser.Tilemap.TILED_JSON);
+		game.load.spritesheet('tilesheet', 'assets/img/WTspritesheetR.png', 32, 32); 
+		game.load.spritesheet('banktile', 'assets/img/WTspritesheetBank.png', 32, 32);
+		game.load.spritesheet('grasstile', 'assets/img/WTspritesheetG.png', 32, 32);
 	},
 	create: function() {
 		var endButtonText = game.add.text(game.world.width-60,10, 'end', {fontSize: '24px', fill: '#ffffff'});
 		endButtonText.inputEnabled = true;
 		endButtonText.events.onInputDown.add(endTapped, this);
 
-		boba = new Boba(game, game.world.centerX, game.world.centerY, 'boba0002');
+		boba = new Boba(game, 100, 200, 'boba0002');
+		boba.scale.setTo(.4, .4);
 
 		var healthText = game.add.text(20, 15, 'health: 100', {fontSize: '24px', fill: '#ffffff'});
 		var moneyText = game.add.text(20, 50, 'money: 100', {fontSize: '24px', fill: '#ffffff'});
+
+		game.physics.startSystem(Phaser.Physics.ARCADE);
+
+		game.stage.setBackgroundColor('#87CEEB'); 
+
+		map = game.add.tilemap('levelOne');
+
+		map.addTilesetImage('WTspritesheetR', 'tilesheet');
+		map.addTilesetImage('WTspritesheetBank', 'banktile');
+		map.addTilesetImage('WTspritesheetG', 'grasstile')
+
+		map.setCollisionByExclusion([]);
+
+		mapLayer = map.createLayer('Roads');
+		bankLayer = map.createLayer('Bank');
+		grassLayer = map.createLayer('Grass');
+
+		mapLayer.resizeWorld();
 	},
 	render: function() {
 		game.debug.body(boba);
