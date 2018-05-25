212c1244e0918634eb34e97e395a205bb63a60a4
lokada <lyokada@ucsc.edu>
2018-05-25 12:20:19 -0700

    ammo text added for LazyT

--- game/js/prefabs/Towers/lazyT.js
+++ game/js/prefabs/Towers/lazyT.js
@@ -1,12 +1,12 @@
 function LazyT(game, x, y, frame, health, ammo, attack, atkrange, atkspeed){
 	Tower.call(this, game, x, y, frame, 10, 6, 3, atkrange, 0, atkspeed);
 
+	this.ammoText = game.add.text(0, 0, "ammo: " + this.ammo, {fontSize: '24px', fill: '#ffffff'});
 }
 
 LazyT.prototype = Object.create(Tower.prototype);
 LazyT.prototype.constructor = LazyT;
 
 LazyT.prototype.update = function(){
-	 
-
+	 this.ammoText.text = "ammo: " + this.ammo;
 }
