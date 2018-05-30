c12aeda2ad62ada72225ff2c070da03d8019d7c3
lokada <lyokada@ucsc.edu>
2018-05-29 18:40:05 -0700

    boba body bounds set

--- game/js/prefabs/boba.js
+++ game/js/prefabs/boba.js
@@ -5,6 +5,7 @@ function Boba(game, x, y, spriteName) {
 	this.attack = 10;
 
 	Enemy.call(this, game, x, y, spriteName);
+	this.body.setSize(0,90,80,30);
 	if (x < 0) {
 		this.speed = 100;
 		this.body.velocity.x = this.speed;
